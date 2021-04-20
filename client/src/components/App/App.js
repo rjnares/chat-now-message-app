c6860babf8c1a778e1eaeac887b123061d951a38
rjnares <roberto.nares@gmail.com>
2021-04-19 20:40:41 -0700

    Implemented SignIn so user can enter ID or create new UUID

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import SignIn from "../SignIn/SignIn";
 
 const App = () => {
-  return <SignIn />;
+  const [id, setId] = useState("");
+
+  return (
+    <React.Fragment>
+      {id ? <h3>ID: {id}</h3> : null}
+      <SignIn onIdSubmit={setId} />
+    </React.Fragment>
+  );
 };
 
 export default App;
