bb4cd4289407d574a7719f3894609a37cd9d5534
rjnares <roberto.nares@gmail.com>
2021-04-20 21:56:03 -0700

    Integrated Express.js to existing Socket.IO server for future use with MongoDB and verified app still works end-to-end

---
+++ README.md
@@ -0,0 +1,4 @@
+# Useful Resources
+
+- Getting started with socket.IO: https://socket.io/get-started/chat
+- Handling CORS: https://socket.io/docs/v3/handling-cors/
--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
-const io = require("socket.io")(5000, {
+// Create HTTP Server (for API URL Endpoints)
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+// Integrate Socket.IO and enable Cross-Origin Resource Sharing (CORS)
+const io = require("socket.io")(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
 
-io.on("connection", (socket) => {
-  const id = socket.handshake.query.id;
-  socket.join(id);
+// Simple route handler to test if server
+// is running on localhost:5000
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Server</h1>");
+});
 
+// Listen on the 'connection' event for incoming sockets
+io.on("connection", (socket) => {
+  // Retrieve ID of connected user from socket
+  const id = socket.handshake.query.id;
+
+  // Subscribe socket of connected user to its own room channel
+  // (which is simply their user ID) and log connected user
+  socket.join(id);
+  console.log(`Connected UID: ${id}`);
+
+  // Broadcast sent message to all sockets (I THINK)
   socket.on("send-message", ({ recipients, text }) => {
     recipients.forEach((recipient) => {
       const newRecipients = recipients.filter((r) => r !== recipient);
@@ -20,4 +40,14 @@ io.on("connection", (socket) => {
       });
     });
   });
+
+  // On disconnect, sockets leave all channels automatically
+  socket.on("disconnect", () => {
+    console.log(`Disconnected UID: ${id}`);
+  });
+});
+
+// Set server to listen on localhost:5000
+server.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
 });
