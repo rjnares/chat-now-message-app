c4514e0ffe3ad56a569b0684f3c7b9847706d00d
rjnares <roberto.nares@gmail.com>
2021-04-23 15:22:57 -0700

    Added navbar with signout button

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -3,13 +3,17 @@ import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import OpenConversation from "../OpenConversation/OpenConversation";
 import { useConversations } from "../../contexts/ConversationsProvider";
+import NavBar from "../NavBar/NavBar";
 
 const Dashboard = () => {
   const { selectedConversation } = useConversations();
   return (
-    <div className="d-flex" style={{ height: "100vh" }}>
-      <Sidebar />
-      {selectedConversation && <OpenConversation />}
+    <div className="d-flex flex-column">
+      <NavBar />
+      <div className="d-flex" style={{ height: "90vh" }}>
+        <Sidebar />
+        {selectedConversation && <OpenConversation />}
+      </div>
     </div>
   );
 };
